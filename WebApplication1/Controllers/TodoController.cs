493c8b6998e1b1c609af6e5d1a2af042e7d0688f
Andrew <andrew.klyachkin@gmail.com>
2020-01-13 16:35:51 +0200

    PUT added, idk why someone would ever need that tho

--- WebApplication1/Controllers/TodoController.cs
+++ WebApplication1/Controllers/TodoController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Controllers
 {
@@ -31,6 +32,35 @@ namespace WebApplication1.Controllers
             _context.Todos.Add(item);
             _context.SaveChanges();
             return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, Todo todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_context.Todos.Find(id) == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
         [HttpDelete("{id}")]
